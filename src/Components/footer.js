688238675fca933b35458ab4ed4ac030d1031f32
ibrahim <aprahim.master@gmail.com>
2024-10-06 10:33:24 +0300

    Ibrahim Abdelhalim Update 3

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,74 +1,63 @@
 import { GiFalconMoon } from "react-icons/gi";
-import Button from 'react-bootstrap/Button';
-
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { IoLogoLinkedin } from "react-icons/io";
 import { FaYoutube } from "react-icons/fa";
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+
+
 function Footer() {
-    return ( 
+      return (
 
-<>
-<div className="col align-self-end text-center justify-content-center" style={{ alignContent:"end", width:"100%",  backgroundColor:"azure", }}>
+            <>
+                  <div className="col align-content-end text-center justify-content-centerw-100 " style={{ backgroundColor: "azure" }}>
 
+                        <h1 className="mb-5 m-3 text-center fs-1 fw-bold " > <GiFalconMoon />Task Manegar</h1>
+                        <h2 className="m-5  text-center ">subscribe to our website</h2>
+                        <div className="  d-flex justify-content-center align-items-center ">
+                              <div className="input-group mb-3 col-md-3 w-50 " >
+                                    <input type="text" className="form-control  " placeholder="your email" aria-label="Recipient's username" aria-describedby="button-addon2"></input>
+                                    <button className="btn " type="button" id="button-addon2" style={{ backgroundColor: "#6b71e0" }}>Subscribe</button>
+                              </div>
+                        </div>
 
-<h1 className="mb-5 m-3" style={{fontSize:"3rem" ,fontWeight:'bold',textAlign:"center"}}> <GiFalconMoon />Task Manegar</h1>
- <h2 className="m-5  text-center ">subscribe to our website</h2>
+                        <div className="row fs-4  d-flex justify-content-center align-items-center">
+                              <div className="col " >
+                                    <h3 className="fs-2">Product</h3>
+                                    <a className="text-decoration-none d-block" style={{ color: 'black' }} href="#">Features</a>
+                                    <a className="text-decoration-none d-block" style={{ color: 'black' }} href="#">Pricing</a>
 
-<div className="row d-flex justify-content-center m-5">   
-<InputGroup className="mb-3">
-  <Form.Control
-    placeholder= "your email"/>
-  <Button variant="success" id="button-addon2">
-    subscribe
-  </Button>
-</InputGroup >
-</div>
+                              </div>
+                              <div className="col " >
+                                    <h3 className="fs-2">Resources</h3>
+                                    <a className="text-decoration-none d-block" style={{ color: 'black' }} href="#">Blog</a>
+                                    <a className="text-decoration-none d-block" style={{ color: 'black' }} href="#">User guides</a>
+                                    <a className="text-decoration-none d-block" style={{ color: ' black' }} href="#">Webinars</a>
+                              </div>
+                              <div className="col " >
+                                    <h3 className="fs-2">Company</h3>
+                                    <a className="text-decoration-none d-block" style={{ color: 'black' }} href="#">Contact us</a>
+                              </div>
 
-<div className="row" style={{display:"flex",flexFlow:"wrap col",justifyContent:"space-evenly",alignItems:"center"}}>
-<div className="col " >
-<h3>Product</h3>
-      <a style={{ textdecoration: 'none',display: 'block',color: 'black'}} href="#">Features</a>
-      <a style={{ textdecoration: 'none',display: "block",color: 'black'}} href="#">Pricing</a>
-     
-     </div>
-<div className="col " >
-        <h3>Resources</h3>
-        <a style={{ textdecoration: 'none',display: 'block',color: 'black'}} href="#">Blog</a>
-        <a style={{ textdecoration: 'none',display: "block",color: 'black'}} href="#">User guides</a>
-        <a style={{ textdecoration: "none",display:' block',color:' black'}}href="#">Webinars</a>
-      </div>
-      <div className="col " >
-      <h3>Company</h3>
-      <a style={{ textdecoration: 'none',display: 'block',color: 'black'}} href="#">Contact us</a>
-      </div>
-<br></br>
-<br></br>
-<div >
-<p style={{textdecoration: 'none',color: 'black'}}>
-<br></br>
-© 2024 Brand. inc <a style={{textdecoration: 'none',color: 'black'}}  href="#"> • Privacy </a> <a style={{textdecoration: 'none',color: 'black'}} href="#">• Terms </a> <a  style={{textdecoration: 'none',color: 'black'}} href="#">• Sitemap </a>
-</p>
-<div className=" col m-3">
-<a  href="#" style={{ textdecoration: 'none',display: 'inline',color: 'black'}}>  <FaTwitter style={{color:"#1DA1F2"}}/> </a>
+                              <div >
 
-<a  href="#" style={{ textdecoration: 'none',display: 'inline',color: 'black'}}> <FaFacebook style={{color:"#1877F2"}}/> </a>
+                                    <div className=" col m-3">
+                                          <a href="#" className="text-decoration-none d-inline" style={{ color: 'black' }}>  <FaTwitter style={{ color: "#1DA1F2" }} /> </a>
 
-<a  href="#" style={{ textdecoration: 'none',display: 'inline',color: 'black'}}> <IoLogoLinkedin style={{color:"#0077B5"}}/> </a>
+                                          <a href="#" className="text-decoration-none d-inline" style={{ color: 'black' }}> <FaFacebook style={{ color: "#1877F2" }} /> </a>
 
-<a  href="#" style={{ textdecoration: 'none',display: 'inline',color: 'black'}}> <FaYoutube style={{color:"#FF0000"}}/>  </a>
-</div>
-</div>
+                                          <a href="#" className="text-decoration-none d-inline" style={{ color: 'black' }}> <IoLogoLinkedin style={{ color: "#0077B5" }} /> </a>
 
-      </div>
+                                          <a href="#" className="text-decoration-none d-inline" style={{ color: 'black' }}> <FaYoutube style={{ color: "#FF0000" }} />  </a>
+                                    </div>
+                                    <p >
+                                          © 2024 Brand. inc <a className="text-decoration-none" style={{ color: 'black' }} href="#"> • Privacy </a> <a className="text-decoration-none" style={{ color: 'black' }} href="#">• Terms </a> <a className="text-decoration-none" style={{ color: 'black' }} href="#">• Sitemap </a>
+                                    </p>
+                              </div>
+                        </div>
+                  </div>
 
+            </>
+      );
+}
 
-
-
-</div>
-</>
-    );}
-
-    export default Footer;
+export default Footer;
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -7,34 +7,34 @@ const Contact = () => {
   return (
     <>
 
-    <div  className=' container mb-5 mt-5 pb-5 font-weight-bold text-center ' > <h1 >Contact Us</h1>
+      <div className=' container mb-5 mt-5 pb-5  text-center ' > <h2 className='fw-bold fs-1'>Contact Us</h2>
 
-<p className="text-justify" style={{fontSize:"1rem",fontWeight:"490"}}>We'd love to hear from you! Please fill out the form below and We 'll get in touch.</p>
-<div className='row m-0 '>
-<div className=' col-md-12  p-0 pt-4 m-auto'>
-  <div className='row d-flex justify-content-center'>
-    <div className='col-md-6 '>
-      <div className='mb-3 '>
-<input className=' form-control mb-3'  placeholder=' Name' ></input>
-<input className=' form-control mb-3' placeholder=' Email' ></input>
-<input className=' form-control mb-3' placeholder=' Phone'  ></input>
-<input className=' form-control mb-3' placeholder=' Project Budget'></input>
-<input className=' form-control mb-3' placeholder=' service Recuired'></input>
-<textarea className=' form-control mb-3' rows="3" placeholder=' Message'  ></textarea>
-</div>
-</div>
-</div>
-</div>
-</div>
-<div className=' m-3 me-5'>
-<a className=' m-2' href='#'><Button variant="success" > <GrSend /> Send Message </Button></a>
+        <p className="fs-6   " >We'd love to hear from you! Please fill out the form below and We 'll get in touch.</p>
+        <div className='row m-0 '>
+          <div className=' col-md-12  p-0 pt-4 m-auto'>
+            <div className='row d-flex justify-content-center'>
+              <div className='col-md-6 '>
+                <div >
+                  <input className=' form-control mb-3' placeholder=' Name' ></input>
+                  <input className=' form-control mb-3' placeholder=' Email' ></input>
+                  <input className=' form-control mb-3' placeholder=' Phone'  ></input>
+                  <input className=' form-control mb-3' placeholder=' Project Budget'></input>
+                  <input className=' form-control mb-3' placeholder=' service Recuired'></input>
+                  <textarea className=' form-control mb-3' rows="3" placeholder=' Message'  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=' m-3 me-5'>
+          <a className=' m-2' href='#'><Button style={{ backgroundColor: "#6b71e0" }} > <GrSend /> Send Message </Button></a>
 
-<a href='#'><Button variant="outline-success"> <PiArrowUDownLeftBold /> Reset</Button></a>
-</div>
+          <a href='#'><Button style={{ backgroundColor: "#6b71e0" }}> <PiArrowUDownLeftBold /> Reset</Button></a>
+        </div>
 
-    </div>
+      </div>
 
-    <Footer />
+      <Footer />
 
     </>
   )
